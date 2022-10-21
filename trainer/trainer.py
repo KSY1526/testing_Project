729231716fa537ac01e8cf85b380906894a42958
ksy98 <zan05134@naver.com>
2022-10-21 14:45:57 +0900

    wandb 간단히 적용. 기록은 이상하지만 잘 되는듯 \n 사용법은 댓글로 담

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,9 +1,10 @@
 import numpy as np
+import wandb
 import torch
 from torchvision.utils import make_grid
 from base import BaseTrainer
 from utils import inf_loop, MetricTracker
-import wandb
+
 
 class Trainer(BaseTrainer):
     """
